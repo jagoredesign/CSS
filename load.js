2238587556558e5dac5cb86b9d3467e3b066c32f
jagoredesign <68729184+jagoredesign@users.noreply.github.com>
2020-08-03 19:22:17 +0700

    Create load.js

---
+++ load.js
@@ -0,0 +1,28 @@
+/* Copyright (c) 2020 Jagoredesign.com
+ * By Gyy Siregar
+/*
+.mega-wrapper,.post-title,.postmeta,.creditpic{display:none}
+body {background:white;overflow: auto;}
+#tab-demo,#view{width:100%;left:0}
+.item .blog-post{float:left;width:100%;height:100%;overflow:unset;position:fixed}
+#view{padding:0;margin:0;border:0;position:fixed;top:25px;left:0;bottom:0;overflow-y:scroll;overflow-x:scroll;width:100%;height:calc(100% - 50px);background:url(https://3.bp.blogspot.com/-wzBkpPn10ZE/XQGf1uMPIvI/AAAAAAAAAlk/QSjhBnJMnbM-vS-vez9HDgRb2S1kyaOKQCLcBGAs/s1600/Cube-1s-200px.gif) center center no-repeat;transition:all .4s ease-out;background-color:#fff;z-index:1}
+#content-wrapper > .container{margin:0}
+#content-wrapper{margin-top:10px}
+#tab-demo{height:60px;top:0;background:#fff;color:#fff;font:400 13px Arial,sans-serif;position:fixed}
+.dlbutton:hover{background:url(//3.bp.blogspot.com/-MHVCwQQDhzQ/UuC7dgZiqvI/AAAAAAAAGV0/llC0hES500M/s1600/download.png) 15px 50% no-repeat #579c26}
+.dlbutton,a.dlbutton{background:url(//3.bp.blogspot.com/-MHVCwQQDhzQ/UuC7dgZiqvI/AAAAAAAAGV0/llC0hES500M/s1600/download.png) 15px 50% no-repeat;padding:0 20px 0 55px;right:158px;text-decoration:none}
+ul.resize-tool,ul.resize-tool li{list-style:none}
+ul.resize-tool{display:inline;float:left;margin:0 0 0 50px!important}
+ul.resize-tool li{display:inline;float:left;height:50px;line-height:50px;margin:0 20px 0 0}
+ul.resize-tool li svg{vertical-align:middle}
+ul.resize-tool li a:active svg path,ul.resize-tool li a:focus svg path{fill:red}
+.w1024{width:1024px!important;height:600px!important;position:absolute!important;margin-bottom:50px!important}
+.w960{width:600px!important;height:960px!important;position:absolute!important;margin-bottom:50px!important}
+.w600{width:960px!important;height:600px!important;position:absolute!important;margin-bottom:50px!important}
+.w414{width:414px!important;height:736px!important;position:absolute!important;margin-bottom:50px!important}
+.w736{width:736px!important;height:414px!important;position:absolute!important;margin-bottom:50px!important}
+.w320{width:320px!important;height:480px!important;position:absolute!important;margin-bottom:50px!important}
+.w480{width:480px!important;height:320px!important;position:absolute!important;margin-bottom:50px!important}
+@media screen and (max-width:1024px){#view{left:-20px}}
+@media screen and (max-width:768px){#view{left:-5px}}
+@media screen and (max-width:667px){#view{top: -25px;height: calc(100% - 88px)}}
